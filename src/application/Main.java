76728da2b2029d82c5004171eefde2b2dcb5933b
Akash Kumar Sikarwar <akash.sky.heaven@gmail.com>
2017-08-02 16:31:12 +0530

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+			Player player = new Player("file:///E:/downloads/Video/anime/tok.mp4");
+			Scene scene = new Scene(player,1280,720,Color.BLACK);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
